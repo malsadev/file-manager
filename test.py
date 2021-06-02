02fd975a3f6444e7e2de65033af565df075da804
Mustafa <Mustafa@10.0.0.244>
2021-06-02 04:26:44 -0400

    Initial  real commit
    Mostly testing methods and getting the very basic functionalty such as
    moving the specific files to and fro directories.
    
    initialized a dictionary which will hold the key words that correspond
    to each course file

--- test.py
+++ test.py
@@ -1,1 +1,36 @@
-print('Hello world')
+#Program Name: automatic file handler
+#Purpose : automatically moves file from a designated folder to appropriate directories
+#Author : Mustafa Al-Sakkaf
+#Date : June 6, 2021
+
+import os
+import shutil
+
+print(os.getcwd()) #get current working directory
+
+
+dump_folder_path ='C:\\Users\Mustafa\\Desktop\\Download Folder' #browser default download folder
+
+print(os.listdir(dump_folder_path))
+
+
+# this dictionary maps the certain keywords (part of file names) to the corresponding folder which are the 'keys'
+key_words = {
+    'phy1722' : [] , 
+    'fls' : [] ,
+    'mat' : [],
+    'seg' : [] ,
+    'iti' : [] ,
+    'eng' : []}
+
+
+print(key_words)
+target_file = 'download.jpg' 
+
+if os.listdir(dump_folder_path).__contains__('Download.jpg'):
+    print('file found')
+    shutil.move(dump_folder_path + '\\download.jpg', dump_folder_path + '\\PHY1722')
+
+
+
+#def move_file():
