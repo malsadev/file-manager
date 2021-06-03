09530ccd34bbb96ed192c31ebe5e04cba300fe0a
Mustafa <Mustafa@10.0.0.244>
2021-06-02 23:33:09 -0400

    Basic functionality done
    Was able to continously run program and 'catch' specifically named files
    and move them to their corresponding folders

--- file_organizer.py
+++ file_organizer.py
@@ -19,25 +19,32 @@ print(os.listdir(dump_folder_path))
 
 # this dictionary maps the certain keywords (part of file names) to the corresponding folder which are the 'keys'
 key_words = {
-    'phy1722' : [] , 
-    'fls2581' : [] ,
-    'mat1722' : [],
-    'seg2900' : [] ,
-    'iti1520' : [] ,
-    'eng1112' : []
+    'Phy1722' : ['physics.jpg', 'download.jpg', 'download1.jpg'] , 
+    'fls2581' : ['french'] ,
+    'mat1722' : ['mathematics'],
+    'seg2900' : ['seg'] ,
+    'iti1520' : ['information technology'] ,
+    'eng1112' : ['english']
     }
 
+print('l' in 'download.jpg')
 
-print(key_words)
+print(key_words['Phy1722'])
+
 target_file = 'download.jpg' #testing
 
 while True: #this program will have to run as a background process as long as the student account is logged in
-    if os.listdir(dump_folder_path).__contains__('download.jpg'):
-        print('file found')
-        shutil.move(dump_folder_path + '\\download.jpg', dump_folder_path + '\\PHY1722')
+    incoming_files= os.listdir(dump_folder_path)
+    
+    for key in key_words:
         
+        for key_word in key_words[key]: #goes through the list of key words associated with each key
+            if incoming_files.__contains__(key_word):
+                print('file found')
+                shutil.move(dump_folder_path + '\\' + key_word, dump_folder_path + '\\PHY1722')
+                
         
-    time.sleep(300)#only runs every 300 seconds so as to not consume all CPU cycles
+    time.sleep(3)#only runs every 3 seconds so as to not consume all CPU cycles
 
 
 #def move_file():
